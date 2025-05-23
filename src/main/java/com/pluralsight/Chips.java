6d48fcf867081ab5fad578a9ad2c84dc197963b9
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-23 12:48:41 -0400

    added Chips class with code

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -2,4 +2,26 @@ package com.pluralsight;
 
 public class Chips {
 
+    private String type;
+
+    public Chips(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+
+        double chipPrice = 1.50;
+
+        return chipPrice;
+
+    }
+
 }

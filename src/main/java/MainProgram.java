2758e0f98512d55d8c95513bbcad7ca64b301a37
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-23 13:12:48 -0400

    added MainProgram

---
+++ src/main/java/MainProgram.java
@@ -0,0 +1,9 @@
+import com.pluralsight.UserInterface;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        UserInterface uI = new UserInterface();
+        uI.displayHomeScreen();
+    }
+}
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -18,10 +18,39 @@ public class UserInterface {
 
                 System.out.println("=========== Welcome to Deli-cious! ============");
                 System.out.println("");
+
+                System.out.println("Enter your Choice:");
+                System.out.println(" ");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        displayOrderScreen();
+                        break;
+                    case 0:
+                        quit = true;
+
+
+
+                }
+
             }
 
 
         }
+
+        public void displayOrderScreen() {
+
+        }
+
+        public void goHome() {
+
+
+        }
+
+
+
 
 
 }

5d224649f525e34aba90c98551c5d375b3f9ba06
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-23 12:57:20 -0400

    started userInterface class

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
+
+        private Sandwich sandwich;
+        private Scanner scanner;
+
+        public UserInterface() {
+            scanner = new Scanner(System.in);
+        }
+
+        public void displayHomeScreen() {
+
+            boolean quit = false;
+            while (!quit) {
+
+                System.out.println("=========== Welcome to Deli-cious! ============");
+                System.out.println("");
+            }
+
+
+        }
+
+
 }

e5f43d6e54ba678accb481bebe2f1df6b15e253c
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-23 13:53:41 -0400

    Added orderScreen to UserInterface

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -17,7 +17,9 @@ public class UserInterface {
             while (!quit) {
 
                 System.out.println("=========== Welcome to Deli-cious! ============");
-                System.out.println("");
+                System.out.println(" ");
+                System.out.println("1 - New Order");
+                System.out.println("0 - Exit the Deli");
 
                 System.out.println("Enter your Choice:");
                 System.out.println(" ");
@@ -31,17 +33,40 @@ public class UserInterface {
                     case 0:
                         quit = true;
 
-
-
                 }
 
             }
-
 
         }
 
         public void displayOrderScreen() {
 
+            boolean quit = false;
+            while (!quit) {
+
+                System.out.println("==== New Order ====");
+                System.out.println(" ");
+                System.out.println("1 - Add Sandwich");
+                System.out.println("2 - Add Drink");
+                System.out.println("3 - Add Chips");
+                System.out.println("4 - Checkout");
+                System.out.println("0 - Cancel Order");
+
+                System.out.println("Enter your Choice:");
+                System.out.println(" ");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        displayOrderScreen();
+                        break;
+                    case 0:
+                        quit = true;
+
+                }
+
+            }
         }
 
         public void goHome() {

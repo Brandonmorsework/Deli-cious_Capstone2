b614077e4db73f38c3be03d2b15ac1c125bd5cd2
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-23 12:24:56 -0400

    added Meat subclass with code

--- src/main/java/com/pluralsight/Meat.java
+++ src/main/java/com/pluralsight/Meat.java
@@ -1,7 +1,30 @@
 package com.pluralsight;
 
-public class Meat {
+public class Meat extends Topping{
 
+    public Meat(String toppingType) {
+        super(toppingType);
+    }
 
+    @Override
+    public double getPrice(int size) {
+
+        double baseMeatPrice = 0;
+
+        if (size == 4) {
+
+            baseMeatPrice = 1.00;
+
+        } else if (size == 8) {
+
+            baseMeatPrice = 2.00;
+
+        } else if (size == 12) {
+
+            baseMeatPrice = 3.00;
+
+        }
+        return baseMeatPrice;
+    }
 
 }

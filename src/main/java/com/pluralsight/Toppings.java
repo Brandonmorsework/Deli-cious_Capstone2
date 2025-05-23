5a092c7c37811063d28d0095a1e9808427d0b4cd
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-23 11:32:50 -0400

    finished rough draft of Topings Abstract class

--- src/main/java/com/pluralsight/Toppings.java
+++ src/main/java/com/pluralsight/Toppings.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
-public class Toppings {
+public abstract class Toppings {
+
+    private String toppingType;
+
+    public Toppings(String toppingType) {
+        this.toppingType = toppingType;
+    }
+
+    public String getToppingType() {
+        return toppingType;
+    }
+
+    public void setToppingType(String toppingType) {
+        this.toppingType = toppingType;
+    }
+
+   public abstract double getPrice();
+
 }

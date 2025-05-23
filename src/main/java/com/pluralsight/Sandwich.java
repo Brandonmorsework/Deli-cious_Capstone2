2e27815c0a6f4e6eaa3bd71c3652926374372d7e
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-23 10:02:21 -0400

    working sandwich class

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sandwich {
+
+    private int size;
+    private boolean isExtraCheese;
+    private boolean isExtraMeat;
+    private boolean isToasted;
+    private String breadType;
+
+    private ArrayList<Topping> toppings;
+
+    public Sandwich(int size, boolean isExtraCheese, boolean isExtraMeat, boolean isToasted, String breadType) {
+        this.size = size;
+        this.isExtraCheese = isExtraCheese;
+        this.isExtraMeat = isExtraMeat;
+        this.isToasted = isToasted;
+        this.breadType = breadType;
+        this.toppings = new ArrayList<>();
+    }
+
+
+
 }

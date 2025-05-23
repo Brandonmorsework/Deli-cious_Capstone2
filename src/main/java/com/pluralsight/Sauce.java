c05edb11c8c4e992736a1b0d4a94b6a9be1726e0
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-23 11:36:53 -0400

    finished rough draft of sauce subclass

--- src/main/java/com/pluralsight/Sauce.java
+++ src/main/java/com/pluralsight/Sauce.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
-public class Sauce {
+public class Sauce extends Toppings {
+
+    public Sauce(String toppingType) {
+        super(toppingType);
+    }
+
+    @Override
+    public double getPrice() {
+        return 0.0;
+    }
 }

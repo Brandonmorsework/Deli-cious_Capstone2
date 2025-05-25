c48267895e36589bc2646226fc8ecb0c573f8dd8
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-25 16:13:33 -0400

    adding switch cases to addSandwich method

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserInterface {
@@ -54,7 +55,7 @@ public class UserInterface {
                 System.out.println("3 - Add Chips");
                 System.out.println("4 - Checkout");
                 System.out.println("0 - Cancel Order");
-
+                System.out.println(" ");
                 System.out.println("Enter your Choice:");
                 System.out.println(" ");
                 int choice = scanner.nextInt();
@@ -82,6 +83,8 @@ public class UserInterface {
 
     public void addSandwich() {
 
+        ArrayList<Topping> toppingList = new ArrayList<>();
+
         System.out.println("Adding Sandwich to Order:");
         System.out.println(" ");
         System.out.println("Please Select a Sandwich Size:");
@@ -89,6 +92,7 @@ public class UserInterface {
         System.out.println(" 8 - 8 Inch Sandwich");
         System.out.println("12 - 12 Inch Sandwich");
         System.out.println(" ");
+        int userSandwichSize = scanner.nextInt();
 
         System.out.println("Please Select a Bread Type: ");
         System.out.println(" ");
@@ -97,24 +101,87 @@ public class UserInterface {
         System.out.println("3 - Rye");
         System.out.println("4 - Wrap");
         System.out.println(" ");
+        int userBreadType = scanner.nextInt();
+
+
+
+
 
         System.out.println("Do you Want Your Bread Toasted? ( Y / N )");
         System.out.println(" ");
 
-        System.out.println("Please Add Your Toppings. Type 'next' When You Are Finished Adding Toppings");
-        System.out.println("Meat Toppings:");
-        System.out.println("1 - Steak");
-        System.out.println("2 - Ham");
-        System.out.println("3 - Salami");
-        System.out.println("4 - Roast Beef");
-        System.out.println("5 - Chicken");
-        System.out.println("6 - Bacon");
-        System.out.println("Cheese Toppings:");
-        System.out.println("");
+        String userInput = scanner.nextLine();
+
+        while (!userInput.equalsIgnoreCase("next")) {
+
+            System.out.println("Please Add Your Toppings. Type 'next' When You Are Finished Adding Toppings");
+            System.out.println("Meat Toppings:");
+            System.out.println("1 - Steak");
+            System.out.println("2 - Ham");
+            System.out.println("3 - Salami");
+            System.out.println("4 - Roast Beef");
+            System.out.println("5 - Chicken");
+            System.out.println("6 - Bacon");
+
+            int userMeatChoice = scanner.nextInt();
+
+            switch (userMeatChoice) {
+
+                case 1:
+                    new Meat("Steak");
+                    break;
+                case 2:
+                    new Meat("Ham");
+                    break;
+                case 3:
+                    new Meat("Salami");
+                    break;
+                case 4:
+                    new Meat("Roast Beef");
+                    break;
+                case 5:
+                    new Meat("Chicken");
+                    break;
+                case 6:
+                    new Meat("Bacon");
+
+            }
+
+            userInput = "next";
+        }
+
+            System.out.println("Cheese Toppings:");
+            System.out.println("1 - American");
+            System.out.println("2 - Provolone");
+            System.out.println("3 - Cheddar");
+            System.out.println("4 - Swiss");
+
+            int userCheeseChoice = scanner.nextInt();
+
+            switch (userCheeseChoice) {
+
+                case 1:
+                    new Cheese("American");
+                    break;
+                case 2:
+                    new Cheese("Provolone");
+                    break;
+                case 3:
+                    new Cheese("Cheddar");
+                    break;
+                case 4:
+                    new Cheese("Swiss");
+                    break;
+
+            }
+
+            userInput = "next";
+
 
         System.out.println("Do you Want Extra Cheese? ( Y / N )");
         System.out.println(" ");
         System.out.println("Do you Want Extra Meat? ( Y / N )");
+
 
 
         order.addSandwich(sandwich);

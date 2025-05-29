9923473841ce91d68910959f49466cd025f23d44
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-29 10:04:58 -0400

    Added signature sandwich and BLT classes

---
+++ src/main/java/com/pluralsight/ingredients/SignatureSandwiches.java
@@ -0,0 +1,14 @@
+package com.pluralsight.ingredients;
+
+import java.util.List;
+
+public class SignatureSandwiches extends Sandwich {
+
+
+    public SignatureSandwiches(int size, String breadType, boolean isToasted, boolean isExtraCheese, boolean isExtraMeat, List<Topping> toppings) {
+        super(size, breadType, isToasted, isExtraCheese, isExtraMeat, toppings);
+    }
+
+
+
+}
